e4b8eeeb1005b841104f62cf1b4733d845035967
JonathanSise <jhonvs93@hotmail.com>
2026-06-20 17:19:01 -0500

    feat: configurar Tailwind, estructura de carpetas y dependencias base

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'jht-dark': '#1A3B6B',
+        'jht-gold': '#FFD400',
+        'jht-gray': '#E3E4E5',
+        'jht-gray-dark': '#909AB3',
+        'jht-blue-support': '#57678E',
+      }
+    },
+  },
+  plugins: [],
+}
