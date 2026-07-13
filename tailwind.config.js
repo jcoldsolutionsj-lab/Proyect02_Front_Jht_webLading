fb50a399976acbdbb3d8aac7083a1f7e1c37e0ea
JonathanSise <jhonvs93@hotmail.com>
2026-07-12 23:10:00 -0500

    Remover carpeta src y configurar Tailwind CSS para Django

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
+    "./jht_cms/templates/**/*.html",
+    "./jht_cms/apps/**/*.py",
+    "./jht_cms/static_src/**/*.js",
   ],
   theme: {
     extend: {
