edbea479091e2cfea26fc3ab60960d9b770fe78f
JonathanSise <jhonvs93@hotmail.com>
2026-07-25 15:59:40 -0500

    feat: maquetacion completa del modulo de noticias (esqueleto frontend y urls temporales)

--- jht_cms/apps/website/urls.py
+++ jht_cms/apps/website/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'website'
@@ -10,4 +11,8 @@ urlpatterns = [
     path('nosotros/', views.NosotrosView.as_view(), name='nosotros'),
     path('contacto/', views.ContactoView.as_view(), name='contacto'),
     path('landing/', views.LandingView.as_view(), name='landing'),
+    
+    # Rutas de prueba para diseño de Frontend (Noticias)
+    path('noticias/', TemplateView.as_view(template_name='public/noticias.html'), name='noticias'),
+    path('noticias/ejemplo/', TemplateView.as_view(template_name='public/noticia_detalle.html'), name='noticia_detalle'),
 ]
